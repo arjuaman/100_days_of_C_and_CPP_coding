e802e0a30dedb05e4a5fabec20ed719bfe193a32
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2022-02-01 10:03:23 +0530

    Index equals value

---
+++ 99/Index Equals Value/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+public class Solution{
+    public static void main(String[] args){
+        int[] nums = {0, 4, 8};
+        int n = nums.length;
+        int ans = util(nums, n);
+        System.out.println(ans);
+    }
+
+    private static int util(int[] nums, int n) {
+        
+        return 0;
+    }
+}
